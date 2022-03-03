ef65d06066cd616ad0697ecb623f55823da7eb89
bel <denoblesnobility2@gmail.com>
2022-03-03 17:12:35 +0000

    firebase config error solving

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -4,12 +4,14 @@ import { getFirestore } from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig={
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey:'AIzaSyCfurWBl5We8Hk4DQZ-qk3oNlwtxUS_7sY',
+    authDomain: 'fullauthentication-70fa8.firebaseapp.com',
+    projectId: 'fullauthentication-70fa8',
+    storageBucket: 'fullauthentication-70fa8.appspot.com',
+    messagingSenderId:'530378337905',
+    appId: '1:530378337905:web:9d684a1ac5b6092fe0f5d0'
+
+ 
 }
 
 const app = initializeApp(firebaseConfig)
